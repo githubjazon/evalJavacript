69c6d7764a9b52b1c69c4d1ae5080300c09155bd
jazon <jason.sevenants@gmail.com>
2022-10-31 10:19:10 +0100

    "new31oct10h18"

--- assets/js/evalContact.js
+++ assets/js/evalContact.js
@@ -1,50 +1,49 @@
 document.getElementById("form1").addEventListener("submit", function(e) {
 
-
-
-    
     var erreur;
     var nom = document.getElementById("nom");
     var prenom = document.getElementById("prenom");
-    var genre = document.getElementById("genre");
-    var genre2 = document.getElementById("genre2");
-    var date = document.getElementById("date");
+    var masculin = document.getElementById("masculin");
+    var feminin = document.getElementById("feminin");
     var cp = document.getElementById("cp");
-    var adresse = document.getElementById("adresse");
-    var ville = document.getElementById("ville");
     var mail = document.getElementById("mail");
     var sujet = document.getElementById("sujet");
     var question = document.getElementById("question");
     
 
+
+    if(!ckbx.value) {
+        erreur = "Veuillez cocher la case";
+    }
+
     if (!question.value) {
         erreur = "Veuillez renseigner une question";
     }
+
     if (!sujet.value) {
         erreur = "Veuillez renseigner un sujet";
     }
+
     if (!mail.value) {
         erreur = "Veuillez renseigner un mail";
     }
-    if (!ville.value) {
-        erreur = "Veuillez renseigner une ville";
-    }
-    if (!adresse.value) {
-        erreur = "Veuillez renseigner une adresse";
-    }
+
     if (!cp.value) {
         erreur = "Veuillez renseigner un cp";
     }
-    if (!date.value) {
-        erreur = "Veuillez renseigner une date";
-    }
-    if (!genre2.value) {
-        erreur = "Veuillez renseigner un genre";
-    }
-    if (!genre.value) {
-        erreur = "Veuillez renseigner un genre";
-    }
-   
+ 
+
+//ho le bordel...
+   function valid() {
+    
+   }
+if (!masculin || feminin) {
+    erreur = "sex ?";
+}
+
+
+//ho le bordel...
+
     if (!prenom.value) {
         erreur = "Veuillez renseigner un prenom";
     }
@@ -64,7 +63,7 @@ document.getElementById("form1").addEventListener("submit", function(e) {
     }
 
   
-    document.getElementById("reset").location.reload();
+
   
 
        
